bbb4960a752f5a6ed757ca1adf0ce0d2c38c869f
JorgePrieto04 <115139584+JorgePrieto04@users.noreply.github.com>
2022-10-24 01:06:45 +0200

    Update ifSwitch.java

--- src/ifSwitch.java
+++ src/ifSwitch.java
@@ -3,34 +3,34 @@ public class ifSwitch {
     int dia = Integer.parseInt(args [0]);
     switch (dia) {
       case 1:
-        System.out.println("El " + dia + "º día de la semana es Lunes");
+        System.out.println("El " + dia + "º día de la semana es lunes,");
         break;
       case 2:
-        System.out.println("El " + dia + "º día de la semana es Martes");
+        System.out.println("El " + dia + "º día de la semana es martes,");
         break;
       case 3:
-        System.out.println("El " + dia + "º día de la semana es Miércoles");
+        System.out.println("El " + dia + "º día de la semana es miércoles,");
         break;
       case 4:
-        System.out.println("El " + dia + "º día de la semana es Jueves");
+        System.out.println("El " + dia + "º día de la semana es jueves,");
         break;
       case 5:
-        System.out.println("El " + dia + "º día de la semana es Viernes");
+        System.out.println("El " + dia + "º día de la semana es viernes,");
         break;
       case 6:
-        System.out.println("El " + dia + "º día de la semana es Sábado");
+        System.out.println("El " + dia + "º día de la semana es sábado,");
         break;
       case 7:
-        System.out.println("El " + dia + "º día de la semana es Domingo");
+        System.out.println("El " + dia + "º día de la semana es domingo,");
         break;
     }	
-    System.out.println("Por lo que " + finde(dia));
+    System.out.println("por lo que " + finde(dia));
   }
   private static String finde(int dia) {
 		if (dia == 6 || dia == 7) {
-			return "ya es fin de semana";
+			return "ya sería fin de semana.";
 		} else {
-			return "todavía todavía no estamos a fin de semana";
+			return " todavía no sería a fin de semana.";
 		}
   }
 }
